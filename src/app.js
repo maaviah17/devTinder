099102d865155b1e2274b1788f0e6c4c9a42495f
Muawiyah <muawiyah1705@gmail.com>
2025-05-30 00:54:03 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -9,11 +9,22 @@ app.use("/",(req,res,next)=>{
 // middleware is called middleware because they sit in the middle of the request and response cycle. 
 // it passes all the middlewares until it reaches the route handler
 
-app.get("/user", (req,res,next)=>{
-    console.log("handling the route2");
-    res.send("2nd route handler");
-    // next();
+app.get("/getUserData", (req,res)=>{
+    // Logic of DB call and get user data 
+
+    throw new Error("randomjfnjdjfjdf")
+    res.send("User data sent");
 })
+
+app.use("/", (err,req,res,next)=>{
+    if(err){
+        // log your error message
+        res.status(500).send("Something went wrong maaaviahhh ;( ");
+    }else{
+        next();
+    }
+})
+
 
 // app.get("/user",(req,res,next)=>{
 //     console.log("this is the 1st route");
