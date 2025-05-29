fa8c57c1cbcb3cb879209b2a9aa83dcc0a5b72fe
Muawiyah <muawiyah1705@gmail.com>
2025-05-29 10:33:32 +0530

    get | post | del routes

--- src/app1.js
+++ src/app1.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+ 
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName : "Muawiyah", 
+        lastName : "Khalid"
+    });
+})
+
+app.post("/user", (req,res)=>{
+    res.send("data saved to database !!")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted user successfully ;) ")
+})
 
 app.use("/test", (req,res)=>{
     res.send("this is the /test route");
-})
-
-app.use("/hello", (req,res)=>{
-    res.send("this is the /hello route")
 })
 
 app.listen(4010,()=>{
