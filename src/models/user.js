4b171b51608fcc99a3d33b798095187b4b0cbba5
Muawiyah <muawiyah1705@gmail.com>
2025-06-02 11:21:20 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,50 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength : 28,
     },
     lastName : {
         type : String
     },
     emailId : {
-        type : String
+        type : String,
+        lowercase : true,
+        required : true,
+        unique : true,
+        trim : true
     },
     password : {
-        type : String
+        type : String,
+        required : true,
     },
     age : {
-        type : String
+        type : Number,
+        min : 18
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male", "female"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl : { 
+        type : String,
+        default : "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg", 
+    },
+    about : {
+        type : String,
+        default : "This is a default about of the user!"
+    },
+    skills : {
+        type : [String] 
     }
+},{
+    timestamps : true,
 });
 
 const User = mongoose.model("User", userSchema);
