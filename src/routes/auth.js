ca8e4f9df8d02ead9f4009095a76b493678ffa3c
Muawiyah <muawiyah1705@gmail.com>
2025-06-19 15:14:00 +0300

    added cors

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,72 +2,72 @@ const express = require("express");
 const { User } = require("../models/user");
 const authRouter = express.Router();
 const { validateSignUpData } = require("../utils/validation");
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 
-authRouter.post("/signup", async (req,res)=>{
+authRouter.post("/signup", async (req, res) => {
     // console.log(req.body);
-    try{
+    try {
         // validation of data
         validateSignUpData(req);
-        
-        const {firstName, lastName, emailId, password} = req.body;
+
+        const { firstName, lastName, emailId, password } = req.body;
         // encrypting the password
-        const passwordHash = await bcrypt.hash(password,1);
+        const passwordHash = await bcrypt.hash(password, 1);
         console.log(passwordHash);
 
         // Creating a new instance of the user model
         const user = new User({
             firstName,
-            lastName, 
+            lastName,
             emailId,
-            password : passwordHash,
+            password: passwordHash,
         });
         await user.save();
         res.send("User added successfully ✅");
-    }catch(err){
+    } catch (err) {
         res.status(500).send("ERROR : " + err.message);
-    } 
+    }
 })
 
 // user login
-authRouter.post("/login", async (req,res)=>{
+authRouter.post("/login", async (req, res) => {
 
-    try{
+    try {
 
-        const {emailId, password} = req.body;
-        const user = await User.findOne({emailId : emailId});
-        if(!user){
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
             res.status(400).send("WRONG LOGIN CREDENTIALS");
         }
         const isPasswordValid = await user.validatePassword(password)
-        if(isPasswordValid){
-            
+        if (isPasswordValid) {
+
             // create a JWT token   
             const token = await user.getJWT();
             console.log(token);
 
             // add the token to cookie & send back cookie to the user.
             res.cookie("token", token, {
-                expires: new Date(Date.now() + 5*36000000)
+                expires: new Date(Date.now() + 5 * 36000000)
             });
-            res.send("User loggedin successfully");
-        }else{
+            res.send(user)
+        } else {
             res.status(400).send("WRONG LOGIN CREDENTIALS");
         }
 
-    }catch(err){
+    } catch (err) {
         res.status(400).send("something went wrong !!");
     }
 })
 
 // logout user
-authRouter.post("/logout" ,(req,res)=>{
-    try{
+authRouter.post("/logout", (req, res) => {
+    try {
         res.cookie("token", null, {
-            expires : new Date(Date.now()),
+            expires: new Date(Date.now()),
         })
         res.send("user logged out :) ")
-    }catch(err){
+    } catch (err) {
         res.status(400).send("something went wrong !!");
     }
 })
