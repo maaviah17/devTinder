31c3e7be5a1c82d150addad9ecbd7154a956248d
Muawiyah <muawiyah1705@gmail.com>
2025-06-30 22:29:06 +0300

    fixed aws access error

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -15,10 +15,8 @@ if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !pro
 
 
 
-// Set the AWS Region.
 const REGION = process.env.AWS_REGION || "eu-north-1";
 
-// Create SES service object.
 const sesClient = new SESClient({
     region: REGION,
     credentials: {
